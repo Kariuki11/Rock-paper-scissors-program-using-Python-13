b5f492403291541fad16ea4c3d0ce870f3333ff8
Kariuki11 <irungukenneth433@gmail.com>
2024-06-18 17:46:25 +0300

    Changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = 
 
 function playGame(playerChoice){
     const computerChoice = choices[Math.floor(Math.random() * 3)];
